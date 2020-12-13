b229b0dc19b11529fd58066c6994ae43263597ba
nachogg18 <44012745+nachogg18@users.noreply.github.com>
2020-12-13 20:11:44 -0300

    trying static settings

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -153,22 +153,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-#EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
-#EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-#EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-#EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'#location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
